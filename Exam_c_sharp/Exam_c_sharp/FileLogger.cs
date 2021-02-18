41315902e9d7f9386c6a554f2f9ed69524ba11ae
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-02-18 20:31:40 +0600

    Exam

---
+++ Exam_c_sharp/Exam_c_sharp/FileLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Exam_c_sharp
+{
+    public class FileLogger : ILog
+    {
+        public void LogInfo(string road , string mess)
+        {
+            var file = (File.Exists(road));
+
+            using (var stream = File.Open(road, file))
+            {
+                var data = ""
+            }
+        }
+    }
+}
---
+++ Exam_c_sharp/Exam_c_sharp/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Exam_c_sharp
+{
+    class Log
+    {
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
